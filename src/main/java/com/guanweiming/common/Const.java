d3490553b31c27dba3f090f331d2054bc9e834ba
关卫明 <zziaguan@163.com>
2017-06-09 09:49:09 +0800

    修复获取空属性的一些可能出现的异常

--- src/main/java/com/guanweiming/common/Const.java
+++ src/main/java/com/guanweiming/common/Const.java
@@ -11,20 +11,21 @@ import java.util.Set;
 public class Const {
     /**
      * 获取对象的空的属性名
-     * @param source 要检测的对象
+     *
+     * @param source           要检测的对象
      * @param ignoreProperties 手动指定要忽略的属性
      * @return 空属性的集合
      */
     public static Set<String> getNullPropertyNames(Object source, String... ignoreProperties) {
-        if(source==null){
-            return null;
+        if (source == null) {
+            return new HashSet<String>();
         }
         Class clazz = source.getClass();
         Field[] fields = clazz.getDeclaredFields();
 
         Set<String> emptyNames = new HashSet<String>();
 
-        for(Field field:fields){
+        for (Field field : fields) {
             field.setAccessible(true);
             Object value = null;
             try {
@@ -32,12 +33,13 @@ public class Const {
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             }
-            if(value == null){
+            if (value == null) {
                 emptyNames.add(field.getName());
             }
             field.setAccessible(false);
         }
-        emptyNames.addAll(Arrays.asList(ignoreProperties));
+        if (ignoreProperties != null)
+            emptyNames.addAll(Arrays.asList(ignoreProperties));
         return emptyNames;
     }
 }
