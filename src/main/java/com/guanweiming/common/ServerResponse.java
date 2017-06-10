47b425d953fe37cfd16ac8e2594b1cdf858c61c7
关卫明 <zziaguan@163.com>
2017-06-10 15:43:04 +0800

    修正一些内容

--- src/main/java/com/guanweiming/common/ServerResponse.java
+++ src/main/java/com/guanweiming/common/ServerResponse.java
@@ -15,69 +15,79 @@ public class ServerResponse<T> implements Serializable {
     private int status;
     private T data;
 
-    private ServerResponse(int status){
+    private ServerResponse(int status) {
         this.status = status;
     }
+
     private ServerResponse(int status, String msg) {
         this.status = status;
         this.msg = msg;
     }
-    private ServerResponse(int status,T data){
+
+    private ServerResponse(int status, T data) {
         this.status = status;
         this.data = data;
     }
-    private ServerResponse(int status,String msg,  T data) {
+
+    private ServerResponse(int status, String msg, T data) {
         this.msg = msg;
         this.status = status;
         this.data = data;
     }
 
     @JsonIgnore
-    public boolean isSuccess(){
+    public boolean isSuccess() {
         return this.status == ResponseCode.SUCCESS.getCode();
     }
 
-    public String getMsg(){
+    public String getMsg() {
         return msg;
     }
-    public int getStatus(){
+
+    public int getStatus() {
         return status;
     }
-    public T getData(){
+
+    public T getData() {
         return data;
     }
 
 
-    public static <T> ServerResponse<T> createBySuccess(){
+    public static <T> ServerResponse<T> createBySuccess() {
         return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
     }
 
-    public static <T> ServerResponse<T> createBySuccess(T data){
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    public static <T> ServerResponse<T> createBySuccess(T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), data);
     }
 
-    public static  <T> ServerResponse<T> createBySuccessMessage(String msg){
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg);
     }
 
-    public static <T> ServerResponse<T> createBySuccessMessageData(String msg,T data){
-        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    public static <T> ServerResponse<T> createBySuccess(String msg, T data) {
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(), msg, data);
     }
 
-    public static <T> ServerResponse<T> createByFailed(){
+    public static <T> ServerResponse<T> createByError() {
         return new ServerResponse<T>(ResponseCode.ERROR.getCode());
     }
 
-    public static <T> ServerResponse<T> createByFailed(String msg){
-        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),msg);
+    public static <T> ServerResponse<T> createByErrorMessage(String msg) {
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(), msg);
     }
 
-    public static <T> ServerResponse<T> createByNeedLogin(){
-        return new ServerResponse<T>(ResponseCode.NEED_LOGIN.getCode(),ResponseCode.NEED_LOGIN.getDesc());
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode, String msg) {
+        return new ServerResponse<T>(errorCode, msg);
     }
 
-    public static <T> ServerResponse<T> createByIllegalArgument(){
-        return new ServerResponse<T>(ResponseCode.ILLEGAL_ARGUMENT.getCode(),ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+    public static <T> ServerResponse<T> createByNeedLogin() {
+        return new ServerResponse<T>(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
     }
+
+    public static <T> ServerResponse<T> createByIllegalArgument() {
+        return new ServerResponse<T>(ResponseCode.ILLEGAL_ARGUMENT.getCode(), ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+    }
+
 
 }
