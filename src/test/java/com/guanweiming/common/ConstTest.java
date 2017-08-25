ae90c4842a1438ca6167b0106a0bd31b2ed29b2c
关卫明 <zziaguan@163.com>
2017-08-26 00:21:10 +0800

    删除不必要的代码

--- src/test/java/com/guanweiming/common/ConstTest.java
+++ src/test/java/com/guanweiming/common/ConstTest.java
@@ -11,10 +11,7 @@ public class ConstTest {
 
     @Test
     public void getNullPropertyNamesTest(){
-        System.out.println(Const.getNullPropertyNames(new Object(),"id","username"));
-        System.out.println(Const.getNullPropertyNames(new Object(),null));
-        System.out.println(Const.getNullPropertyNames(null,"id","username"));
-        System.out.println(Const.getNullPropertyNames(null,null));
+
     }
 
 }
