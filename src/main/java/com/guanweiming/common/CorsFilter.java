e7f057f20592f8be3f20a98f5dd989940ecd6254
zziaguan <zziaguan@163.com>
2017-12-07 11:30:58 +0800

    过滤器

--- src/main/java/com/guanweiming/common/CorsFilter.java
+++ src/main/java/com/guanweiming/common/CorsFilter.java
@@ -3,18 +3,60 @@ package com.guanweiming.common;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * @author https://github.com/zziaguan/
  */
-public abstract class CorsFilter extends OncePerRequestFilter {
-    protected void doFilterInternal(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response, javax.servlet.FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Origin", "*");
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, javax.servlet.FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Max-Age", getMaxAge());
+        response.setHeader("Access-Control-Allow-Origin", getAllowOrigin());
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+        response.setHeader("Access-Control-Allow-Methods", getAllowMethods());
         response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        long start = System.currentTimeMillis();
+        beforeProcess(request,response);
         filterChain.doFilter(request, response);
+        afterProcess(request,response);
+        long end = System.currentTimeMillis();
+        processTime(end-start);
+
     }
+
+
+
+    protected String getAllowOrigin() {
+        return "*";
+    }
+
+    protected String getMaxAge() {
+        return "3600";
+    }
+
+    protected String getAllowMethods() {
+        return "GET,HEAD,OPTIONS,POST,PUT,DELETE";
+    }
+
+    protected void processTime(final long time){
+
+    }
+
+    /**
+     * 方便用户在处理之前搞些事情
+     */
+    protected void beforeProcess(final HttpServletRequest request,final HttpServletResponse response){
+
+    }
+    /**
+     * 方便用户在处理之后搞些事情
+     */
+    private void afterProcess(final HttpServletRequest request,final HttpServletResponse response) {
+
+    }
+
 }
